005e601ea3d21d03750d1d11e565a66a46eb18ce
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-08 13:33:10 +0900

    v1C102sbb
    create C102sbb
    add Question Class

---
+++ yoojinwon/C201sbb/src/main/java/com/mysite/C201sbb/C201sbbApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.C201sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C201sbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C201sbbApplication.class, args);
+	}
+
+}
