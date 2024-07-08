d89364fe8459beee052e8fc4a71ab943e8f15514
JUN HYUK KWON <sojourn8th@naver.com>
2024-07-08 16:49:00 +0900

    2.1.7

--- kwonjunhyuk/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ kwonjunhyuk/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -11,11 +11,59 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
 @SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	
+	@Test
+	void testJpa13() {
+		Optional<Question>oq=this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q=oq.get();
+		
+		List<Answer> answerList=q.getAnswerList();
+		
+		assertEquals(1,answerList.size());
+		assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
+	}
+	
+	
+	
+	void testJpa12() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a= oa.get();
+		assertEquals(2,a.getQuestion().getId());
+	}
+	
+	
+	
+	
+	
+	void testJpa11() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+	}
+	
+	
 	
 
 	void testJpa() {
@@ -61,7 +109,7 @@ class SbbApplicationTests {
         assertEquals(1, q.getId());
     }
 	
-	@Test
+
 	void testJpa5() {
 		assertEquals(2,this.questionRepository.count());
 		Optional<Question>oq=this.questionRepository.findById(1);
