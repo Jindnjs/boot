9353f499d36142ea10072837e289c43f03509176
Jihwan-P <wlghks9619@gmail.com>
2024-07-08 17:38:17 +0900

    24.7.8.3

--- parkjihwan/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ parkjihwan/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,22 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QuestionController {
 
-		@GetMapping("/question/list")
-		public String list() {
-			return "question_list";
-		}
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@GetMapping("/question/list")
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+		return "question_list";
+	}
 }
--- parkjihwan/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ parkjihwan/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,7 +2,7 @@ package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Optional;
+// import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
