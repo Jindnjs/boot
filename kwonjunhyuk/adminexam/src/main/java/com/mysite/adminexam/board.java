d4cb4a1f75ba52f4a6a07037dedd304582c8b7ab
JUN HYUK KWON <sojourn8th@naver.com>
2024-07-09 13:40:38 +0900

    boot

---
+++ kwonjunhyuk/adminexam/src/main/java/com/mysite/adminexam/board.java
@@ -0,0 +1,18 @@
+package com.mysite.adminexam;
+
+public class board {
+
+	@Autowired
+	id;
+	
+	@Autowired
+	title;
+	
+	@Autowired
+	content;
+	
+	@Autowired
+	date;
+	
+	//이렇게 id, date는 필수적으로 들어가야된다고 함
+}
